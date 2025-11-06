a504b54e53d47ee3e1ed779d6d1b736b9a3d6491
Alex Wills <alex.wills@arthrex.com>
2025-11-06 08:10:48 -0500

    Cleaning pt2

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,8 +1,8 @@
 return {
-	-- Any simple plugins that don't need special specs or configs
+        -- Any simple plugins that don't need special specs or configs
         -- fuzzyfind for file searching
-	{ "junegunn/fzf" },
-	{ "junegunn/fzf.vim" },
+        { "junegunn/fzf" },
+        { "junegunn/fzf.vim" },
         -- template configs for LSPs
         {"neovim/nvim-lspconfig"},
 }
